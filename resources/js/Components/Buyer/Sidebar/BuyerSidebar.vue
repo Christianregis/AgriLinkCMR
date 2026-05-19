89d4648c6c3c4adf41d53f96e1ae9059a151e085
Christianregis <christiancheukap@gmail.com>
2026-05-19 15:12:23 +0100

    Mise a jour du code

--- resources/js/Components/Buyer/Sidebar/BuyerSidebar.vue
+++ resources/js/Components/Buyer/Sidebar/BuyerSidebar.vue
@@ -112,7 +112,7 @@
                 <i class="fas fa-heart w-5 text-center text-neutral-muted group-hover:text-brand-primary"></i>
                 <span class="font-medium">Mes Favoris</span>
             </Link>
-            <Link href="#" @click="closeSidebar"
+            <Link :href="userMessageShow()" @click="closeSidebar"
                 class="flex items-center space-x-3 px-4 py-3 rounded-xl text-neutral-body hover:bg-brand-bg hover:text-brand-primary transition-all group">
                 <i class="fas fa-envelope w-5 text-center text-neutral-muted group-hover:text-brand-primary"></i>
                 <span class="font-medium">Messagerie</span>
